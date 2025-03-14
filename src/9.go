42e5408e31d8d74859983cfdcc8fe364299b6cb2
carrivieth413 <carrivieth413@gmail.com>
2025-03-14 10:32:08 +0000

    Add 9.go

---
+++ src/9.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Example usage of the function
+	result := myFunction(10)
+	fmt.Println(result)
+}
+
+func myFunction(n int) int {
+	// Implement your solution here
+}
